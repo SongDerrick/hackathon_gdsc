613f2c4c4220ae553fb7835a02ba23bd3c886d75
SongDerrick <sd990307@gmail.com>
2023-04-29 21:59:43 +0900

    [SONG] react integrated

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,7 +11,7 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -18,8 +19,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', indexRouter);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/build', 'index.html'));
+});
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
